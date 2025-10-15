c75adde296921829b70a4aa8e17a5ae21913f1a0
Nico Alvarez <nicolas.alv3@gmail.com>
2025-10-15 20:40:25 -0300

    add time interval management to project details

--- src/views/Admin/ProjectDetails.vue
+++ src/views/Admin/ProjectDetails.vue
@@ -1,16 +1,16 @@
 <template>
   <v-container>
-    <BreadCrumb items="projectDetailsPath" />
+    <BreadCrumb items="projectDetailsPath"/>
     <h1 class="mb-6">Detalle del proyecto</h1>
 
     <v-form @submit.prevent="saveProject">
       <!-- Información del proyecto -->
       <v-card class="pa-4 mb-6">
         <h2>Información del Proyecto</h2>
-        <v-text-field label="Nombre del proyecto" v-model="project.name" required />
-        <v-textarea label="Descripción del proyecto" v-model="project.description" required />
-        <v-text-field label="URL de la imagen" v-model="project.image" />
-        <v-text-field label="Sitio web del proyecto" v-model="project.web" />
+        <v-text-field label="Nombre del proyecto" v-model="project.name" required/>
+        <v-textarea label="Descripción del proyecto" v-model="project.description" required/>
+        <v-text-field label="URL de la imagen" v-model="project.image"/>
+        <v-text-field label="Sitio web del proyecto" v-model="project.web"/>
         <v-select
             label="Tipo de adaptación"
             v-model="project.gamificationStrategy"
@@ -21,14 +21,15 @@
             <v-icon @click="showGamificationInfo">mdi-information</v-icon>
           </template>
         </v-select>
-        <v-switch label="Disponible" v-model="project.available" color="green" />
+        <v-switch label="Disponible" v-model="project.available" color="green"/>
       </v-card>
 
       <v-dialog v-model="gamificationDialog" max-width="500px">
         <v-card>
           <v-card-title>¿Qué es una adaptación de ludificación?</v-card-title>
           <v-card-text>
-            <p>La gamificación es una estrategia que utiliza elementos de juego en contextos no lúdicos. En este proyecto, puedes elegir entre:</p>
+            <p>La gamificación es una estrategia que utiliza elementos de juego en contextos no lúdicos. En este
+              proyecto, puedes elegir entre:</p>
             <ul>
               <li><strong>Elástica:</strong> Se ajusta dinámicamente a las necesidades del usuario.</li>
               <li><strong>Básica:</strong> Aplica reglas predefinidas sin adaptación dinámica.</li>
@@ -43,14 +44,15 @@
       <!-- Áreas -->
       <CollapsableSection title="Áreas">
         <p class="text-subtitle-1 mb-3">Define las áreas geográficas del proyecto</p>
-        <GeoMap v-if="project.areas" :area="project.areas" @update-area="updateProjectAreas" />
+        <GeoMap v-if="project.areas" :area="project.areas" @update-area="updateProjectAreas"/>
       </CollapsableSection>
 
       <CollapsableSection title="Tipos de Tareas">
         <p class="text-subtitle-1 mb-3">Tabla de tipos de tareas</p>
         <v-row>
           <v-col cols="10">
-            <v-text-field v-model="newTaskType" label="Añadir nuevo tipo de tarea" placeholder="Escribe un tipo de tarea" />
+            <v-text-field v-model="newTaskType" label="Añadir nuevo tipo de tarea"
+                          placeholder="Escribe un tipo de tarea"/>
           </v-col>
           <v-col cols="2">
             <v-btn color="black" @click="addNewTaskType">Agregar</v-btn>
@@ -76,6 +78,61 @@
           </tbody>
         </v-simple-table>
       </CollapsableSection>
+      <!-- Intervalos de Tiempo -->
+      <CollapsableSection title="Intervalos de Tiempo">
+        <p class="text-subtitle-1 mb-3">Define los intervalos de tiempo y los días aplicables</p>
+
+        <v-btn color="primary" class="mb-4" @click="addNewTimeInterval">
+          Añadir nuevo intervalo de tiempo
+        </v-btn>
+
+        <CollapsableSection style="background: whitesmoke; padding: 2em" v-for="(interval, index) in project.timeIntervals"
+                :key="index" class="mb-4" :title="interval.name || 'Nuevo Intervalo'">
+          <v-text-field label="Nombre del intervalo" v-model="interval.name" required/>
+
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                  label="Hora de inicio"
+                  type="number"
+                  v-model="interval.time.start"
+                  :min="0"
+                  :max="24"
+                  step="1"
+                  required
+              />
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                  label="Hora de finalización"
+                  type="number"
+                  v-model="interval.time.end"
+                  :min="0"
+                  :max="24"
+                  step="1"
+                  required
+              />
+            </v-col>
+          </v-row>
+
+          <h4>Días de la semana</h4>
+          <v-checkbox
+              v-for="day in daysOfWeek"
+              :key="day.value"
+              v-model="interval.days"
+              :label="day.text"
+              :value="day.value"
+              hide-details
+          />
+
+          <v-btn color="red" @click="removeTimeInterval(index)">Eliminar intervalo</v-btn>
+
+          <!-- Mostrar advertencias de validación -->
+          <v-alert v-if="!isValidInterval(interval) && interval.name" type="error" variant="outlined" class="mt-4">
+            El intervalo debe tener un nombre, un horario válido y al menos un día seleccionado.
+          </v-alert>
+        </CollapsableSection>
+      </CollapsableSection>
       <CollapsableSection title="Tareas" @click="taskSectionClick">
       </CollapsableSection>
       <v-btn type="submit" variant="elevated" color="primary" width="100%" :disabled="hasInvalidTimeIntervals">
@@ -86,10 +143,10 @@
 </template>
 
 <script setup>
-import { onMounted, ref, computed } from 'vue';
-import { useRoute } from 'vue-router';
+import {onMounted, ref, computed} from 'vue';
+import {useRoute} from 'vue-router';
 import ProjectsService from '@/services/ProjectsService';
-import { toast } from 'vue3-toastify';
+import {toast} from 'vue3-toastify';
 import CollapsableSection from '@/components/utils/CollapsableSection.vue';
 import GeoMap from "@/views/Admin/GeoMap.vue";
 import BreadCrumb from "@/components/utils/BreadCrumb.vue";
@@ -109,6 +166,16 @@ const project = ref({
   gamificationStrategy: null,
 });
 
+const daysOfWeek = [
+  {value: 1, text: 'Lunes'},
+  {value: 2, text: 'Martes'},
+  {value: 3, text: 'Miércoles'},
+  {value: 4, text: 'Jueves'},
+  {value: 5, text: 'Viernes'},
+  {value: 6, text: 'Sábado'},
+  {value: 7, text: 'Domingo'}
+];
+
 const newTaskType = ref('');
 const isNew = ref(false);
 const gamificationDialog = ref(false);
@@ -118,12 +185,6 @@ const taskSectionClick = () => {
     router.push(`/admin/project/${project._id}/tasks`)
   })
 }
-// Validar si un intervalo de tiempo es válido
-const isValidInterval = (interval) => {
-  return interval.name.trim() !== '' &&
-      interval.time.start >= 0 && interval.time.end > interval.time.start &&
-      interval.days.length > 0;
-};
 
 // Computed para saber si hay intervalos de tiempo inválidos
 const hasInvalidTimeIntervals = computed(() => {
@@ -148,6 +209,25 @@ const removeTaskType = (index) => {
   toast.info('Tarea eliminada');
 };
 
+// Validar si un intervalo de tiempo es válido
+const isValidInterval = (interval) => {
+  return interval.name.trim() !== '' &&
+      interval.time.start >= 0 && interval.time.end > interval.time.start &&
+      interval.days.length > 0;
+};
+
+const addNewTimeInterval = () => {
+  project.value.timeIntervals.push({
+    name: '',
+    time: {start: 0, end: 0},
+    days: []
+  });
+};
+
+const removeTimeInterval = (index) => {
+  project.value.timeIntervals.splice(index, 1);
+  toast.info('Intervalo eliminado');
+};
 
 const saveProject = async () => {
   if (isNew.value) {
@@ -159,8 +239,6 @@ const saveProject = async () => {
   } else {
     return ProjectsService.updateProject(project.value).then((r) => {
       toast.success('Proyecto actualizado exitosamente')
-      return r
-      toast.success('Proyecto actualizado exitosamente');
       return r;
     });
   }
@@ -171,7 +249,18 @@ onMounted(async () => {
   const projectId = route.params.projectId;
   if (projectId === 'new') {
     isNew.value = true;
-    project.value = { _id: '', name: '', description: '', image: '', web: '', available: false, areas: [], taskTypes: [], timeIntervals: [], gamificationStrategy: 'BASICA' };
+    project.value = {
+      _id: '',
+      name: '',
+      description: '',
+      image: '',
+      web: '',
+      available: false,
+      areas: [],
+      taskTypes: [],
+      timeIntervals: [],
+      gamificationStrategy: 'BASICA'
+    };
   } else {
     const p = await ProjectsService.getProjectById(projectId);
     project.value = p;
